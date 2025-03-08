2c8699265a09f703718dfde18782e0214be56eaf
Artem <usmad42@mail.ru>
2025-03-08 21:27:12 +0700

    Добавлен базовый калькулятор

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Простой калькулятор
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# Тестим функции
+print("Сложение: ", add(5, 3))  # Вывод: 8
+print("Вычитание: ", subtract(10, 4))  # Вывод: 6

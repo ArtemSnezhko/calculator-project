d8676b0608412721843cf7bb62787993aa9e535e
Artem <usmad42@mail.ru>
2025-03-08 21:51:07 +0700

    Добавлена функция умножения

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
 # Простой калькулятор
 def add(a, b):
-    return a + b
-
+    return a + b
 def subtract(a, b):
-    return a - b
-
+    return a - b
+def multiply(a, b):
+    return a * b
 # Тестим функции
-print("Сложение: ", add(5, 3))  # Вывод: 8
-print("Вычитание: ", subtract(10, 4))  # Вывод: 6
+print("Сложение: ", add(5, 3))# Вывод: 8
+print("Вычитание: ", subtract(10, 4)) # Вывод: 6
+print("Умножение: ", multiply(2, 3))# Вывод: 6

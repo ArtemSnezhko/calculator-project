9f5d8711f6884333995c75540f28b5709630f495
Artem <usmad42@mail.ru>
2025-03-09 11:08:24 +0700

    "Добавлена функция возведения в степень"

--- calculator.py
+++ calculator.py
@@ -1,15 +1,17 @@
-python
 # Простой калькулятор
 def add(a, b):
-    return a + b
+	return a + b
 def subtract(a, b):
-    return a - b
+	return a - b
 def multiply(a, b):
-    return a * b
+	return a * b
 def divide(a, b):
-    return a / b
+	return a / b
+def power(a, b):
+	return a ** b
 # Тестим функции
-print("Сложение: ", add(5, 3))# Вывод: 8
+print("Сложение: ", add(5, 3))  # Вывод: 8
 print("Вычитание: ", subtract(10, 4)) # Вывод: 6
-print("Умножение: ", multiply(2, 3)) # Вывод: 6
-print("Деление: ", divide(8, 0)) # Ошибка!
+print("Умножение: ", multiply(2, 3))  # Вывод: 6
+print("Деление: ", divide(8, 2))  # Вывод: 4.0
+print("Степень: ", power(2, 3))  # Вывод: 8

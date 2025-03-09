4178b87d7072454bccee8eed6ee97d880f66a435
Artem <usmad42@mail.ru>
2025-03-09 08:44:01 +0700

    Добавлена функция деления

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+python
 # Простой калькулятор
 def add(a, b):
     return a + b
@@ -5,7 +6,10 @@ def subtract(a, b):
     return a - b
 def multiply(a, b):
     return a * b
+def divide(a, b):
+    return a / b
 # Тестим функции
 print("Сложение: ", add(5, 3))# Вывод: 8
 print("Вычитание: ", subtract(10, 4)) # Вывод: 6
-print("Умножение: ", multiply(2, 3))# Вывод: 6
+print("Умножение: ", multiply(2, 3)) # Вывод: 6
+print("Деление: ", divide(8, 2)) # Вывод: 4.0

5631bc3761a1823197603db33372b33d2556603a
Artem <usmad42@mail.ru>
2025-03-09 21:21:52 +0700

    "Добавлена функция квадратного корня"

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-# Изменение с GitHub
+import math
+
 # Простой калькулятор
 def add(a, b):
     return a + b
@@ -18,6 +19,9 @@ def power(a, b):
 def percentage(total, percent):
     return (total * percent) / 100
 
+def square_root(a):
+    return math.sqrt(a)
+
 # Тестим функции
 print("Сложение: ", add(5, 3))      # Вывод: 8
 print("Вычитание: ", subtract(10, 4)) # Вывод: 6
@@ -25,5 +29,5 @@ print("Умножение: ", multiply(2, 3))  # Вывод: 6
 print("Деление: ", divide(8, 2))     # Вывод: 4.0
 print("Степень: ", power(2, 3))      # Вывод: 8
 print("Процент: ", percentage(200, 50)) # Вывод: 100.0
-print("Процент 2: ", percentage(50, 10)) # Вывод: 5.0v
-# Локальное изменение для теста
+print("Процент 2: ", percentage(50, 10)) # Вывод: 5.0
+print("Квадратный корень: ", square_root(16)) # Вывод: 4.0
